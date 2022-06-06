05a55f2aff9f6c94e08c2920193ba6aafebad0e6
Miklós <Mikls@DESKTOP-LDOMVBQ>
2022-06-06 17:44:47 +0200

    Add project files.

---
+++ MovieDataBase/Controllers/MoviesController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieDataBase.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ILogger<MoviesController> _logger;
+        // this is our in memory repo of movies (?)
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(ILogger<MoviesController> logger, IMovieRepository movieRepository)
+        {
+            _logger = logger;
+            _movieRepository = movieRepository;
+            _logger.LogInformation($"Movies ctor initialized: {GetHashCode()}");
+        }
+
+        [HttpGet("movies")]
+        public List<Movie> GetMovies()
+        { 
+            _logger.LogInformation($"getmovies was called...");
+            return _movieRepository.GetAllMovies();
+        }
+
+        [HttpPost("addmovie")]
+        public void Post([FromBody]string title)
+        {
+            _logger.LogInformation($"Post1 was called to add movie titled \"{title}\".");
+            _movieRepository.Add(title);
+        }
+
+        [HttpDelete("removemovie")]
+        public void Delete([FromBody]string title)
+        {
+            foreach(Movie movie in _movieRepository.GetAllMovies())
+            {
+                if(movie.Equals(title))
+                {
+                    _movieRepository.Remove(movie);
+                }
+            }
+        }
+    }
+}
---
+++ MovieDataBase/Services/IMovieRepository.cs
@@ -0,0 +1,51 @@
+﻿public interface IMovieRepository
+{
+    public List<Movie> GetAllMovies();
+    public void Add(string title);
+
+    public void Remove(Movie movie);
+}
+
+public class Movie 
+{
+    public string Name { get; set; }
+        
+    public Movie(string name)
+    {
+        Name = name;
+        var id = Guid.NewGuid();
+    }
+
+    public bool Equals(string title)
+    {
+        return Name == title;
+    }
+}
+
+public class InMemoryMovieRepository : IMovieRepository
+{
+    private List<Movie> _movies = new List<Movie>();
+
+    public InMemoryMovieRepository()
+    {
+        _movies.Add(new Movie("Kung Fury"));
+        _movies.Add(new Movie("Last Action Hero"));
+    }
+
+    public void Add(string title)
+    {
+        Movie movie = new Movie(title);
+        _movies.Add(movie);
+    }
+
+    public List<Movie> GetAllMovies()
+    {
+        return _movies;
+    }
+
+    public void Remove(Movie movie)
+    {
+        _movies.Remove(movie);
+    }
+}
+
