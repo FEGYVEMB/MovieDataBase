df01bda977e33603af28164bc69118e3f97ecf16
FEGYVEMB <fegyveres.miklos@gmail.com>
2022-09-07 13:41:48 +0200

    changes: - applied naming conventions for asynchronous -GetMovieById: added exception handling

--- MovieDataBase/Services/IMovieRepository.cs
+++ MovieDataBase/Services/IMovieRepository.cs
@@ -1,10 +1,10 @@
 ﻿
 public interface IMovieRepository
 {
-    public Task<List<Movie>> GetAllMovies();
-    public Task<Movie> GetMovieById(string id);
-    public void Add(string title);
-    public void RemoveMovie(string id);
-    public void UpdateMovie(string id, string updatedTitle);
+    public Task<List<Movie>> GetAllMoviesAsync();
+    public Task<Movie> GetMovieByIdAsync(string id);
+    public void AddAsync(string title);
+    public void RemoveMovieAsync(string id);
+    public void UpdateMovieAsync(string id, string updatedTitle);
     public Task<bool> MovieExists(string id);
 }
