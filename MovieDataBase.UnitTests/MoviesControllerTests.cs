9633c1a6360b6c6d77345d75b0578355bd6fcbc9
Miklos Fegyveres <karoly.farter@gmail.com>
2022-09-15 13:42:38 +0200

    changes: - added Unit Testing project in separate branch

---
+++ MovieDataBase.UnitTests/MoviesControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using MovieDataBase.Controllers;
+
+namespace MovieDataBase.UnitTests;
+
+public class MoviesControllerTests
+{
+    private readonly MoviesController _sut;
+
+    [Fact]
+    public void GetMovies_ShouldReturnAllMovies_WhenCalled()
+    {
+
+    }
+}
